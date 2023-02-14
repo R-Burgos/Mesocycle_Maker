93c3ae78c59ebedfe75ee57890cc1066a61eaed2
Ruben Burgos <code.rburgos@gmail.com>
2023-02-14 10:45:31 -0600

    Add project files.

---
+++ Mesocycle_Maker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mesocycle_Maker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
